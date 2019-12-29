ec5e3cd0f61b8434fa4dcd5707ff07118ed8b52f
Bianca Nilfa <ramsolucoes@gmail.com>
2019-12-28 21:08:15 -0300

    Create a drawer navigation

--- src/routesRoot.js
+++ src/routesRoot.js
@@ -1,14 +1,21 @@
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-
+import { createDrawerNavigator } from 'react-navigation-drawer';
 //pages
 import Home from './pages/home';
 
 
 //Monta uma pilha de navegação para tratar as telas de Autenticação
-const AppStack = createStackNavigator({
-    Home: Home
-})
+const AppStack = createDrawerNavigator(
+    {
+        Home: Home
+    },
+    {
+        initialRouteName: 'Home',
+        contentOptions: {
+            activeTintColor: '#e91e63',
+        }
+    }
+)
 
 //Monta a navegação com o Drewer menu
 const AppRoot = createAppContainer(AppStack)
