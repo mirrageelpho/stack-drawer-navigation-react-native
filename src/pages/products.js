e4d85053730b9a8cce77e89fbf85377cda79c960
Bianca Nilfa <ramsolucoes@gmail.com>
2019-12-28 21:11:44 -0300

    Create products page

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function DetailPage() {
+export default function Products() {
     return(
         <View style={css.container}>
-            <Text>Detail Page</Text>
+            <Text>Products</Text>
         </View>
     )
 }
--- src/routesRoot.js
+++ src/routesRoot.js
@@ -2,12 +2,13 @@ import { createAppContainer } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 //pages
 import Home from './pages/home';
-
+import Products from './pages/products';
 
 //Monta uma pilha de navegação para tratar as telas de Autenticação
 const AppStack = createDrawerNavigator(
     {
-        Home: Home
+        Home: Home,
+        Products: Products
     },
     {
         initialRouteName: 'Home',
